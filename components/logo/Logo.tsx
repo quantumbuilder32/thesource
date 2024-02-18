3897ca61dabeeefbe3b4ec4636ef023e631ce5c9
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-02-18 00:08:35 -0500

    new stuff

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 
 export default function Logo() {
     return (
-        <Link href={"/"}>
-            <Image className={`${styles.logo}`} alt='the Source Logo' priority={true} src={require(`@/public/logo light.png`).default.src} width={250} height={250} style={{ objectFit: "contain", margin: "0 auto" }} />
+        <Link href={"/"} className={`${styles.logo}`}>
+            <Image alt='the Source Logo' priority={true} src={require(`@/public/logo light.png`).default.src} width={250} height={250} style={{ objectFit: "contain", margin: "0 auto", cursor: "pointer" }} />
         </Link>
     )
 }
