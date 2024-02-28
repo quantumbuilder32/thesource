9a1dbdb0385a843eca1eaec8911dce9edc6af361
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-02-27 22:07:56 -0500

    nice

--- app/contactUs/page.tsx
+++ app/contactUs/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
             </section>
 
             <section style={{ paddingBlock: "4rem" }}>
-                <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(min(200px, 100%), 1fr))", gap: "1rem", paddingInline: "1rem" }}>
+                <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(min(200px, 100%), 1fr))", gap: "1rem" }}>
                     {[
                         {
                             svg: <svg style={{ width: "2rem", fill: "#fff" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" /></svg>,
@@ -42,7 +42,7 @@ export default function Page() {
                 </div>
             </section>
 
-            <section style={{ paddingInline: "2rem" }}>
+            <section>
                 <ContactForm />
             </section>
         </div>
