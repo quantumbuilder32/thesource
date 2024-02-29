8a0ae6edf1050bb59c8814fb36864e7ee15a3dae
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-02-28 20:02:31 -0500

    nice

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="noScrollBar snap" style={{ flex: "5 1 300px", display: "grid", gridAutoFlow: "column", gridAutoColumns: "300px", overflowX: "auto", color: "#000", gap: "1rem" }}>
+        <div className="snap" style={{ flex: "5 1 300px", display: "grid", gridAutoFlow: "column", gridAutoColumns: "300px", overflowX: "auto", color: "#000", gap: "1rem" }}>
           {servicesSummaryData.map((eachService, eachServiceIndex) => {
             return (
               <div key={eachServiceIndex} style={{ display: "flex", flexDirection: "column", gap: "1rem", alignContent: "flex-start", paddingInline: "1rem" }}>
@@ -119,7 +119,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section style={{ backgroundColor: "var(--backgroundColor)" }}>
+      {/* <section style={{ backgroundColor: "var(--backgroundColor)" }}>
         <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem", margin: "0 auto", maxWidth: "1100px" }}>
           <div style={{ flex: "4 1 400px", display: "flex", alignItems: "flex-start", gap: ".5rem" }}>
             <svg style={{ fill: "var(--fadedColor)", width: "4rem" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" /></svg>
@@ -135,7 +135,7 @@ export default function Home() {
             <Link href={""} style={{ marginBlock: "2rem", display: "inline-block", color: "var(--fadedColor)" }}>More Testimonials</Link>
           </div>
         </div>
-      </section>
+      </section> */}
     </main>
   );
 }
