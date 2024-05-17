b4f04271727a971a3ba890854ad54c620556d5d2
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-05-17 06:55:04 -0500

    nice email update

--- components/EmailTemplate.tsx
+++ components/EmailTemplate.tsx
@@ -13,7 +13,10 @@ export type userForm = Z.infer<typeof userFormSchema>
 export const EmailTemplate = ({ name, email, company, message }: userForm) => (
     <div>
         <h1>{name} has contacted you!</h1>
+
         {company && <h2>Company: {company}</h2>}
+
+        <p>Email: {email}</p>
 
         <p>{message}</p>
     </div>
